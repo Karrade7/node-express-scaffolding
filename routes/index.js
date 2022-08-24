1aaf673eee2d5d4d336e8fb92e3e8097cdd77d34
Jessvin Thomas <jessvin.thomas@skoutsecure.com>
2022-08-24 09:04:28 -0600

    formatted all code files with prettier

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,57 @@
 // requirements for the entire app
-'use strict';
-const config = require('config');
-const logger = require('winston'); // Logger being used
-const path = require('path'); // module to read paths on the server
-const errorHandler = require(path.join(ABSOLUTE_PATH, 'util/errorHandler.js')); // load error routines
+"use strict";
+const config = require("config");
+const logger = require("winston"); // Logger being used
+const path = require("path"); // module to read paths on the server
+const errorHandler = require(path.join(ABSOLUTE_PATH, "util/errorHandler.js")); // load error routines
 
 // Requirements for this script
-const express = require('express'); // Common Web application development for NodeJS
-const passport = require('passport');
+const express = require("express"); // Common Web application development for NodeJS
+const passport = require("passport");
 const router = express.Router();
-const { check, validationResult } = require('express-validator'); // used to validate inputs to routes
-const isAuthenticated = require(path.join(ABSOLUTE_PATH, 'util/authentication.js'));
-
+const { check, validationResult } = require("express-validator"); // used to validate inputs to routes
+const isAuthenticated = require(path.join(ABSOLUTE_PATH, "util/authentication.js"));
 
 // GET home page.
-router.get('/', isAuthenticated, function (req, res, next) {
+router.get("/", isAuthenticated, function (req, res, next) {
   logger.info("Home page route");
-  res.render('index', { title: 'Home'});
+  res.render("index", { title: "Home" });
 });
 
 // GET login page.
-router.get('/login', function (req, res, next) {
+router.get("/login", function (req, res, next) {
   logger.info("Login Page - Get");
-  res.render('login', { title: 'Login'});
+  res.render("login", { title: "Login" });
 });
 
 // POST login page.
-router.post('/login',
+router.post(
+  "/login",
   [
-    check('username').isLength({min: 1}).withMessage('Username is required').trim(),
-    check('password').isLength({min: 1}).withMessage ('Password is required').trim(),
+    check("username").isLength({ min: 1 }).withMessage("Username is required").trim(),
+    check("password").isLength({ min: 1 }).withMessage("Password is required").trim(),
   ],
- passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', })
-)
+  passport.authenticate("local", { successRedirect: "/", failureRedirect: "/login" })
+);
 
-router.use('/logout', function(req, res){
+router.use("/logout", function (req, res) {
   req.logout();
-  res.clearCookie('connect.sid');
-  res.redirect('/');
+  res.clearCookie("connect.sid");
+  res.redirect("/");
 });
 
-router.get('/oauth2', passport.authenticate('oauth2'))
+router.get("/oauth2", passport.authenticate("oauth2"));
 
-router.get('/oauth/cognito', passport.authenticate('oauth2', { scope: ['email', 'openid', 'aws.cognito.signin.user.admin', 'profile'], failureRedirect: '/failure' }), function (req, res) {
-  // Successful authentication, redirect home.
-  res.redirect('/')
-})
+router.get(
+  "/oauth/cognito",
+  passport.authenticate("oauth2", {
+    scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+    failureRedirect: "/failure",
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
 
 module.exports = router;
-
--- util/expressapp.js
+++ util/expressapp.js
@@ -1,14 +1,13 @@
 // Common Modules used in all modules
-'use strict';
-const config = require('config'); // Load config first
-const logger = require('winston'); // load winston as logger
-const path = require('path'); // module to read paths on the server - this is not icluded as const only on entry
-const errorHandler = require(path.join(ABSOLUTE_PATH, 'util/errorHandler.js')); // load error routines
-
+"use strict";
+const config = require("config"); // Load config first
+const logger = require("winston"); // load winston as logger
+const path = require("path"); // module to read paths on the server - this is not icluded as const only on entry
+const errorHandler = require(path.join(ABSOLUTE_PATH, "util/errorHandler.js")); // load error routines
 
 // Requirements for this script
-const express = require('express'); // Common Web application development for NodeJS
-const passport = require('passport'); // Commonly used authenticaton middleware for NodeJS
+const express = require("express"); // Common Web application development for NodeJS
+const passport = require("passport"); // Commonly used authenticaton middleware for NodeJS
 
 /*
 Create Express App
@@ -27,7 +26,7 @@ Things to remember:
 var app = express();
 
 // Middleware - Log all http requests
-app.use( (req, res, done) => {
+app.use((req, res, done) => {
   logger.info(req.originalUrl);
   done();
 });
@@ -39,68 +38,64 @@ app.use(express.json());
 // Accept url-encoded information and add to req object for example host.com?key=value
 app.use(express.urlencoded({ extended: false }));
 
-
 // set view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Layout middle ware (from express-ejs-layouts)
 // this will cause res.render(file) to call layout.ejs first and and render "file" into the layout.ejs file at body
-const expressLayouts = require('express-ejs-layouts'); // make ejs more repeatable with a repeatable header and footer
+const expressLayouts = require("express-ejs-layouts"); // make ejs more repeatable with a repeatable header and footer
 app.use(expressLayouts);
 
 // make certain paths that are not in views accessible by client browser
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // setup authentication
 // Configure authentication
-require(path.join(ABSOLUTE_PATH, 'util/authentication.js'));
+require(path.join(ABSOLUTE_PATH, "util/authentication.js"));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // setup sessions using memory store
 const session = require("express-session"); // needed to store sessions and related cookies
-const MemoryStore = require('memorystore')(session) // a production stable session store in memory, in particular will expire old sessions
+const MemoryStore = require("memorystore")(session); // a production stable session store in memory, in particular will expire old sessions
 const crypto = require("crypto");
-app.use(session({
-  name: 'sessionId',
-  cookie: { secure: true, httpOnly: true, maxAge: 86400000 },
-  saveUninitialized: false,
-  store: new MemoryStore({checkPeriod: 86400000}), // prune expired entries every 24h
-  resave: false,
-  secret: crypto.randomBytes(8).toString('hex'), // this create a random secret. note this is more secure, but it means sessions won't persist among clustered servers
-}))
-
+app.use(
+  session({
+    name: "sessionId",
+    cookie: { secure: true, httpOnly: true, maxAge: 86400000 },
+    saveUninitialized: false,
+    store: new MemoryStore({ checkPeriod: 86400000 }), // prune expired entries every 24h
+    resave: false,
+    secret: crypto.randomBytes(8).toString("hex"), // this create a random secret. note this is more secure, but it means sessions won't persist among clustered servers
+  })
+);
 
 // Setup the router by getting all the routes and adding to the middleware
-const indexRouter = require(path.join(ABSOLUTE_PATH, 'routes/index'));
-app.use('/', indexRouter);
-
-
+const indexRouter = require(path.join(ABSOLUTE_PATH, "routes/index"));
+app.use("/", indexRouter);
 
 // If you got this far, means you've called a route we don't have
 // Mark  as a 404 and forward to error handler
 // ideas from: https://github.com/expressjs/express/blob/master/examples/error-pages/index.js
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404);
 
   // respond with html page
-  if (req.accepts('html')) {
-    res.render('errors/404', { url: req.url });
+  if (req.accepts("html")) {
+    res.render("errors/404", { url: req.url });
     return;
   }
 
   // respond with json
-  if (req.accepts('json')) {
-    res.json({ error: 'Not found' });
+  if (req.accepts("json")) {
+    res.json({ error: "Not found" });
     return;
   }
 
   // default to plain-text. send()
-  res.type('txt').send('Not found');
+  res.type("txt").send("Not found");
 });
-
-
 
 // Setup error handler by specifying it as the last middle ware
 app.use(errorHandler);
