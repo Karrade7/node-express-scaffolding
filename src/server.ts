de3f5beda89312bddbf4931b27118fbf8332d80c
Jessvin Thomas <jessvin.thomas@skoutsecure.com>
2022-11-29 17:59:45 -0700

    upgraded packages and added dotenv

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ declare global {
 }
 global.ABSOLUTE_PATH = path.resolve(__dirname);
 
+// Configure environment variables
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 // Common Modules used in all modules
 process.env["NODE_CONFIG_DIR"] = path.join(ABSOLUTE_PATH, "config");
 const appConfig = require("config"); // Load config first
